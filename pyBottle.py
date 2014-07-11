2c89af0f8f14efc22123905aeece61b63ecca8b1
LISTERINE <jferretti6890@gmail.com>
2014-07-10 20:03:53 -0400

    small edits

--- pyBottle.py
+++ pyBottle.py
@@ -7,33 +7,33 @@ from socket import socket, gethostbyname, gethostname, AF_INET, SOCK_STREAM
 from time import sleep, localtime, strftime
 
 
-def get_time():
-    return  int(strftime("%H", localtime()))
+
 
 def time_between(minTime, maxTime):
-    curTime = get_time()
+    curTime = int(strftime("%H", localtime()))
     return (curTime <= minTime or curTime >= maxTime)
 
-def power_signal(state):
-    print state, get_time()
-    if state == "home" and time_between(8, 16):
+def timer():
+    if time_between(8, 16):
         call(["br", "--on=1,2"])
-    elif state == "away":
+    else:
         call(["br", "--off=1,2"])
 
-
+def signal(command):
+    command = "br "+command
+    call(command.split())
 
 
 server = socket(AF_INET, SOCK_STREAM)
-server.bind(("10.0.0.111", 5000))
-server.listen(1)
+server.bind(("", 5000))
+server.listen(5)
 
 while 1:
 
     print "Waiting for data"
     client = server.accept()
     data = client[0].recv(1024)
-    power_signal(data)
+    signal(data)
 
 
 server.close()
